f1e280a5c495c03a916648f553d111f34a49342b
Dmitrii Reshetnikov <chipsoft2@gmail.com>
2022-02-24 16:12:23 +0400

    First commit

---
+++ src/com/company/home/Hello.java
@@ -0,0 +1,7 @@
+package com.company.home;
+
+public class Hello {
+    public void test (){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/com/company/home/Main.java
@@ -0,0 +1,11 @@
+package com.company.home;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Hello s1 = new Hello();
+        s1.test();
+
+    }
+}
